b6fe9a52d7de45e2380e266b434fe238a8acf3c0
Yoga Setiawan <mryoga1995@gmail.com>
2019-05-11 13:55:31 +0700

    Initial commit

---
+++ soal_no_1.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ari oki
+ * Date: 11/05/2019
+ * Time: 13.41
+ */
+
+biodataSaya();
+
+function biodataSaya(){
+    $biodata = array{
+        "name" => "Yoga Setiawan",
+
+    };
+}
