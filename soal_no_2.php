7d0475c8217abf64700f817c091066283c52bd8a
Yoga Setiawan <mryoga1995@gmail.com>
2019-05-11 16:04:16 +0700

    Soal No 2

--- soal_no_2.php
+++ soal_no_2.php
@@ -5,8 +5,10 @@
  * Date: 11/05/2019
  * Time: 13.41
  */
-print_r(is_username_valid("yog.yoga"));
-//print_r(is_email_valid("yoga@yoga.com"));
+//print_r(is_username_valid("yog.yoga"));
+
+
+print_r(is_email_valid("akudia|@kamu.com"));
 
 function is_username_valid ($username){
 
@@ -21,5 +23,13 @@ function is_username_valid ($username){
 }
 
 function is_email_valid($email){
+     $cek_format = preg_match('/^([A-Za-z0-9\.]*)\@([A-Za-z][A-Za-z0-9\.]*)(\.[A-Za-z0-9\.]*)+$/', $email);
+     $jumlah_username = strlen(explode("@",$email)[0]);
 
+     if ($jumlah_username >= 4 && $cek_format){
+         return true;
+     }else{
+         return false;
+     }
 }
+?>
